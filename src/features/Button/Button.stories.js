4e08490800c68d63ffcad2e699bdb5e30bd2c694
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-06-30 22:50:51 +0900

    Feat: Button 스토리 추가 및 props 추가

--- src/features/Button/Button.stories.js
+++ src/features/Button/Button.stories.js
@@ -1,22 +1,49 @@
 import React from "react";
 import Button from "./Button";
-import { mainColor, subColor } from "../../styled";
 
 export default {
   title: "Feature/Button",
   component: Button,
   argTypes: {
-    color: {
-      name: "color",
+    name: {
+      name: "button",
       type: { name: "string", required: true },
-      description: "example111",
-      defaultValue: "white",
+      description: "naming the button",
+      defaultValue: "Button",
     },
     border: {
       name: "border",
       type: { name: "string", required: false },
-      description: "example122",
+      description: "handling the border of the button",
       defaultValue: "none",
+    },
+    color: {
+      name: "color",
+      type: { name: "string", required: true },
+      description: "handling the color of the button",
+      defaultValue: "white",
+    },
+    size: {
+      name: "size",
+      type: { name: "select", required: true },
+      description: "handling the size of the button",
+      defaultValue: "large",
+      options: ["small", "middle", "large", "xlarge", "2xlarge"],
+    },
+    tabIndex: {
+      name: "tabIndex",
+      type: { name: "number", required: true },
+      description: "handling the tabindex property of the button",
+      defaultValue: 0,
+    },
+    onClick: {
+      name: "onClick",
+    },
+    onMouseEnter: {
+      name: "onMouseEnter",
+    },
+    onMouseLeave: {
+      name: "onMouseLeave",
     },
   },
   parameters: {
@@ -29,25 +56,65 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 
 Primary.args = {
-  width: 100,
-  height: 50,
-  bgColor: mainColor,
+  bgColor: "#0e62fe",
+  hover: {
+    bgColor: "#024fe5",
+  },
+  active: {
+    bgColor: "#022b9d",
+  },
 };
 
-export const Secondary = Template.bind({});
+export const Danger = Template.bind({});
 
-Secondary.args = {
-  width: 100,
-  height: 50,
-  bgColor: subColor,
+Danger.args = {
+  bgColor: "#da1e28",
+  hover: {
+    bgColor: "#b91823",
+  },
+  active: {
+    bgColor: "#760c11",
+  },
 };
 
 export const Ghost = Template.bind({});
 
 Ghost.args = {
-  width: 100,
-  height: 50,
   bgColor: "white",
-  border: `1px solid black`,
-  color: "black",
+  hover: {
+    bgColor: "#e8e8e8",
+  },
+  active: {
+    bgColor: "#c5c5c5",
+    border: "1px solid white",
+  },
+  color: "#0e62fe",
+};
+
+export const Secondary = Template.bind({});
+
+Secondary.args = {
+  bgColor: "#393939",
+  hover: {
+    bgColor: "#424242",
+  },
+  active: {
+    bgColor: "#6e6e6e",
+  },
+};
+
+export const Tertiary = Template.bind({});
+
+Tertiary.args = {
+  bgColor: "transparent",
+  hover: {
+    bgColor: "#024fe5",
+    color: "white",
+  },
+  active: {
+    bgColor: "#022b9d",
+    color: "white",
+  },
+  color: "#0e62fe",
+  border: "1px solid #0e62fe",
 };
--- src/features/Button/Button.tsx
+++ src/features/Button/Button.tsx
@@ -3,35 +3,83 @@ import { css } from "@emotion/react";
 import React from "react";
 
 const container = (
-  width: number,
-  height: number,
   bgColor: string,
+  hover: { border: string; bgColor: string; color: string },
+  active: { border: string; bgColor: string; color: string },
   border: string,
-  color: string
+  color: string,
+  size: string
 ) => css`
-  width: ${width}px;
-  height: ${height}px;
+  height: ${size === "small"
+    ? "32px"
+    : size === "middle"
+    ? "40px"
+    : size === "large"
+    ? "48px"
+    : size === "xlarge"
+    ? "64px"
+    : size === "2xlarge"
+    ? "80px"
+    : "32px"};
   display: flex;
-  justify-content: center;
   align-items: center;
+  padding: 3px 60px 3px 12px;
   border: ${border};
-  border-radius: 10px;
   background-color: ${bgColor};
+  font-size: 14px;
   color: ${color};
+  :hover {
+    border: ${hover.border};
+    background-color: ${hover.bgColor};
+    color: ${hover.color};
+  }
+  :focus {
+    border-width: 4px;
+    border-style: double;
+  }
+  :active {
+    border: ${active.border};
+    background-color: ${active.bgColor};
+    color: ${active.color};
+  }
 `;
 
 type ButtonProps = {
-  width: number;
-  height: number;
+  name: string;
   bgColor: string;
+  hover: { border: string; bgColor: string; color: string };
+  active: { border: string; bgColor: string; color: string };
   border: string;
   color: string;
+  size: string;
+  tabIndex: number;
+  onClick: () => void;
+  onMouseEnter: () => void;
+  onMouseLeave: () => void;
 };
 
-const Button = ({ width, height, bgColor, border, color }: ButtonProps) => {
+const Button = ({
+  name,
+  bgColor,
+  hover,
+  active,
+  border,
+  color,
+  size,
+  tabIndex,
+  onClick,
+  onMouseEnter,
+  onMouseLeave,
+}: ButtonProps) => {
   return (
-    <button css={container(width, height, bgColor, border, color)}>
-      Button
+    <button
+      css={container(bgColor, hover, active, border, color, size)}
+      tabIndex={tabIndex}
+      onClick={onClick}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {name}
     </button>
   );
 };
